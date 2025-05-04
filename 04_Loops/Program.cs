cccb0b392f1a94af0add8e05be4e4a95e42baec5
Hasan Mert Gür <23291274@ogrenci.ankara.edu.tr>
2025-05-04 22:21:25 +0300

    4. dersin bitmesi

---
+++ 04_Loops/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            #region For Loop
+
+            //for(int i = 0; i < 10; i++) // C'dekiyle aynı mantıkta kullanılır
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Console.Write("Yazmak istediğiniz adedi girniz: ");
+            //int adet = int.Parse(Console.ReadLine());
+            //for (int i = 0; i < adet; i++)
+            //{
+            //    Console.WriteLine("Yaşasın Cumhuriyet");
+            //}
+
+
+
+
+
+
+            Console.Read();
+            #endregion
+
+
+            #region For and Desicions
+
+            //for (int i = 1; i <=100; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //int bacterium = 1;
+            
+            //for(int i = 0; i<=24; i++) {
+            //    bacterium *= 2;
+            //    Console.WriteLine(i+". saat sonunda bakteri sayısı: " + bacterium);
+            //}
+
+
+            Console.Read();
+            #endregion
+
+
+
+            #region While Loop
+
+            //int i = 0;
+            //while (i <= 10)
+            //{
+            //    Console.WriteLine("Hello Loops");
+            //    i++;
+            //}
+
+            //int i = 1;
+            //int sum = 0;
+
+            //while (i <= 10)
+            //{
+
+            //    sum += i;
+            //    i++;
+            //}
+            //Console.WriteLine(sum);
+
+
+
+
+            //Console.Read();
+            #endregion
+
+
+            #region Örnek
+
+
+            //Console.Write("Bir sayı giriniz: ");
+            //int num = int.Parse(Console.ReadLine());
+            //int ones,tens, hundreds;
+            //int sum;
+            //ones = num % 10;
+            //hundreds = num / 100;
+            //tens = (num % 100) / 10;
+            //sum = ones + hundreds + tens;
+            //Console.WriteLine("Birler bas: "+ones+" Onlar bas: "+tens+" Yüzler bas: "+hundreds+" Baas top: "+sum);
+
+
+
+            //Console.Read();
+            #endregion
+        }
+    }
+}
