00defe5dea7b55cf522ad78da569721befa228bf
Hasan Mert Gür <23291274@ogrenci.ankara.edu.tr>
2025-05-15 16:21:00 +0300

    SQL ve C# bağlantısının bitişi

---
+++ 08_SQL/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_SQL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ado.net kütüphanesi kullanırız
+
+            Console.WriteLine("***** C# Veri Tabanlı Ürün-Kategori Bilgi Sistemi *****");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            string tableNum;
+
+
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("1-Kategoriler");
+            Console.WriteLine("2-Ürünler");
+            Console.WriteLine("3-Siparişler");
+            Console.WriteLine("4-Çıkış");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("Lütfen getirmek istediğiniz tablo numaarasını giriniz: ");
+            tableNum = Console.ReadLine();
+            Console.WriteLine("--------------------------------------------");
+
+
+            SqlConnection connection = new SqlConnection("Data Source=EYCEMCI\\SQLEXPRESS; initial Catalog=CSharpEgitim;" +
+                " integrated security=true");
+            // Data Source: Sql Server'ın ismi
+            // initial Catalog: Sql Server'da hangi veri tabanını kullanacağımızı belirtiyoruz
+            // integrated security: Sql Server'a bağlanmak için Windows kimlik bilgilerini kullanacağımızı belirtiyoruz yani true
+            // SqlConnection bir class'tır
+            // connection bu classtan türetilen bir instance(nesne)'dır
+
+
+            SqlCommand command = new SqlCommand("Select * From Table_1",connection);
+            // SqlCommand classından command instance'ı oluşturduk içine de yapmak istediğimiz query'i yazdık
+            // hangi databese'e bağlanacağımızı SqlConnection class'ından türettiğimiz connection instance'ı ile belirttik
+
+
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            // bu kodlarımızla sql arasında bir köprü olur
+
+
+            DataTable table = new DataTable();
+            // bize ramden bir alan sağladı
+
+
+            adapter.Fill(table);
+            // adapter ile table'ı doldurduk
+
+            connection.Close();
+            // connection'ı kapatıyoruz
+
+            foreach (DataRow row in table.Rows)
+            {
+                // DataRow: Sql'den gelen satırları temsil eder
+                // data table'dan gelen bir sınıf türüdür
+
+                foreach (var item in row.ItemArray)  // var tüm değişken türlerini kapsar
+                                                     // table'ın satrılarını tek tek geziyoruz item ile
+                                                     // rowdan gelen içeriğin satırlarını okuruz row.ItemArray ile
+                {
+                    Console.Write(item.ToString());    // ToString: item'ı string'e çevirir
+                }
+                Console.WriteLine();                // her satırdan sonra bir alt satıra geçiyoruz
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ 09_SQL_CRUD/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_SQL_CRUD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("***** Menü Sipariş Paneli *****");
+            //Console.WriteLine();
+            //Console.WriteLine("---------------------");
+
+            #region Tek bir sütuna ekleme yapma
+
+            //Console.Write("Eklemek istediğiniz kategori adı: ");
+            //string categorNeme = Console.ReadLine();
+
+            //SqlConnection connection = new SqlConnection("Data Source=EYCEMCI\\SQLEXPRESS; initial catalog=CSharpEgitim; integrated security=true");
+            //connection.Open();  // bağlantıyı açtık
+
+            //SqlCommand cmd = new SqlCommand("insert into Table_1 (CategoryName) values (@p1)",connection);
+            //// insert into eklemeye yarar
+            //// Table_1 işlem yapacağımız tablodur
+            //// CategoryName ekleme ayapılacak sütundur
+            //// p1'den değer al
+            //// connection ekledik veri tabanını belirtmek için
+
+            //cmd.Parameters.AddWithValue("@p1",categorNeme);
+            //// bizim vereceğimiz değeri atadık
+            //cmd.ExecuteNonQuery();
+            //// koşulsuz şekilde sorguyu çalıştır
+
+            //connection.Close();
+
+            //Console.WriteLine("Kategori başarıyla eklendi");
+
+            #endregion
+
+
+            #region Birden Fazla Sütuna Ekleme Yapma
+
+
+            // string productName;
+            // decimal price;
+
+            // Console.Write("Eklemek istediğiniz ürün: ");
+            // productName = Console.ReadLine();
+            // Console.Write("Eklemek istediğiniz ürünün fiyatı: ");
+            // price = decimal.Parse(Console.ReadLine());
+
+
+            //SqlConnection connection = new SqlConnection("Data Source=EYCEMCI\\SQLEXPRESS; initial catalog=CSharpEgitim; integrated security=true");
+            //connection.Open();
+            // SqlCommand command = new SqlCommand("insert into Table_2 (ProductName,ProductPrice,ProductStatus) values (@p1,@p2,@p3)", connection);
+            // command.Parameters.AddWithValue("@p1",productName);    
+            // command.Parameters.AddWithValue("@p2",price);    
+            // command.Parameters.AddWithValue("@p3",true);    
+
+            // command.ExecuteNonQuery();
+
+            // connection.Close();
+            // Console.WriteLine("Başarıyla eklendi");
+
+
+            #endregion
+
+            #region Ürün Lİsteleme
+
+            //SqlConnection connection = new SqlConnection("Data Source=EYCEMCI\\SQLEXPRESS; initial Catalog=CSharpEgitim;" +
+            //    " integrated security=true");
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("Select * From Table_2", connection);
+            //SqlDataAdapter adapter = new SqlDataAdapter(command);
+            //DataTable dataTable = new DataTable();
+            //adapter.Fill(dataTable);
+
+            //foreach (DataRow row in dataTable.Rows)
+            //{
+            //    foreach (var item in row.ItemArray)
+            //    {
+
+            //        Console.Write(item.ToString() + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //connection.Close();
+
+            #endregion
+
+            #region Ürün Silme
+
+            //Console.Write("Silinecek ürünün id'si: ");
+            //int productID = int.Parse(Console.ReadLine());
+
+            //SqlConnection connection = new SqlConnection("Data Source=EYCEMCI\\SQLEXPRESS; initial Catalog=CSharpEgitim;" +
+            //    " integrated security=true");
+
+            //connection.Open();
+
+            //SqlCommand command = new SqlCommand("Delete From Table_2 Where ProductId=@productID", connection);
+            //// delete komutu silmeye yarar
+            //// @ ile değer veririz
+
+            //command.Parameters.AddWithValue("@productID", productID);
+            //command.ExecuteNonQuery();
+            //connection.Close();
+
+            //Console.WriteLine("Silme işlemi başarıyla gerçekleşti");
+
+            #endregion
+
+
+            #region Ürün Güncelleme
+
+            //Console.Write("Güncellenecek ürünün id'si: ");
+
+            //int productID = int.Parse(Console.ReadLine());
+            //Console.Write("Güncellenecek ürünün adı: ");
+            //string productName = Console.ReadLine();
+            //Console.Write("Güncellenecek ürünün fiyatı: ");
+            //decimal productPrice = decimal.Parse(Console.ReadLine());
+
+
+            //SqlConnection connection = new SqlConnection("Data Source=EYCEMCI\\SQLEXPRESS; initial Catalog=CSharpEgitim;" +
+            //   " integrated security=true");
+
+            //connection.Open();
+
+            //SqlCommand command = new SqlCommand("Update Table_2 Set ProductName=@p1,ProductPrice=@p2 Where ProductId=@p3", connection);
+            //// update komutu güncellemeye yarar
+            //// set ile ayarlarız
+            //// where ile hangi id'yi güncelleyeceğimizi belirtiyoruz
+            //command.Parameters.AddWithValue("@p1", productName);
+            //command.Parameters.AddWithValue("@p2", productPrice);
+            //command.Parameters.AddWithValue("@p3", productID);
+            //command.ExecuteNonQuery();
+
+            //connection.Close();
+
+            //Console.WriteLine("Güncelleme işlemi başarıyla gerçekleşti");
+            //Console.Read();
+
+
+            #endregion
+        }
+
+
+    }
+    }
+
